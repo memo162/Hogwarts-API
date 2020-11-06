5aa2ff479935cdd9a3908a6c758e7b0af86ba03c
memo162 <jgespinosa91@gmail.com>
2020-11-06 18:34:30 -0500

    HOW-23: adding applicationAdmision class and base class

--- Hogwarts-API/Models/ApplicationAdmision.cs
+++ Hogwarts-API/Models/ApplicationAdmision.cs
@@ -2,7 +2,12 @@
 
 namespace Models
 {
-    public class Request
+    public class ApplicationAdmision : Base
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DocumentNumber { get; set; }
+        public int Age { get; set; }
+        public string House { get; set; }
     }
 }
---
+++ Hogwarts-API/Models/Base.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class Base
+    {
+        public Guid Id { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+    }
+}
